f97c1d65f3e7694a88f6033296bc2365032aff44
Doniyor-Umurzakov <your-email@example.com>
2024-10-08 23:37:25 +0500

    first commit Epi_help

---
+++ lib/Aplication.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        title: "Epi_Help",
+        theme: ThemeData(
+          useMaterial3: true, // Enable Material 3 for modern UI
+          // Additional theme configurations can go here
+        ),
+        home:,
+        debugShowCheckedModeBanner: false, // Hide the debug banner
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:epi_help/Aplication.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const Application());
+}
